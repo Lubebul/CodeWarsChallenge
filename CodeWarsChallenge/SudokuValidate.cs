53e5317dcc5653341e00e75af0ba1439248b8d57
Lubebul <davide.lubo@gmail.com>
2020-10-23 15:43:34 +0200

    Nuova classe SudokuValidate

---
+++ CodeWarsChallenge/SudokuValidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsChallenge
+{
+    class SudokuValidate
+    {
+        public static bool ValidateSolution(int[][] board)
+        {
+            //TODO
+
+
+        }
+    }
+}

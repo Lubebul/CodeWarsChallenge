d9fc8077ef7b4586cbec9d8632231c7b5ebf5252
Lubebul <davide.lubo@gmail.com>
2020-10-26 17:01:34 +0100

    Reset Test

--- CodeWarsChallenge/SudokuValidate.cs
+++ CodeWarsChallenge/SudokuValidate.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
 using System.Text;
 
 namespace CodeWarsChallenge
@@ -9,7 +11,43 @@ namespace CodeWarsChallenge
         public static bool ValidateSolution(int[][] board)
         {
             //TODO
+            var riga= new List<int>();
+            var colonna= new List<int>();
+            
+            for (int i = 0; i < board.Length; i++){
+                riga.Clear();
+                colonna.Clear();
+                riga.Add(0);
+                colonna.Add(0);
+                for (int j = 0; j < board[0].Length; j++) {
 
+                    if (riga.Contains(board[i][j]) || colonna.Contains(board[j][i]))
+                        return false;
+                    riga.Add(board[i][j]);
+                    colonna.Add(board[j][i]);
+                }
+            }
+
+            var square= new List<int>();
+
+            for (int k = 1; k < 4; k++){
+                for (int z = 1; z < 4; z++) {
+                    square.Clear();
+                    for (int i = 3 * (k - 1); i < (3 * k) - 1; i++)
+                    {
+                        for (int j = 3 * (z - 1); j < (3 * z) - 1; j++)
+                        {
+
+                            if (square.Contains(board[i][j]))
+                                return false;
+                            square.Add(board[i][j]);
+
+                        }
+                    }
+                }
+            }
+
+            return true;
 
         }
     }
